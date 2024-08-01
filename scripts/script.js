835b295b35e948335abbe5f5286e4ab30d0e858f
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2024-08-01 02:47:37 -0300

    feat: inclui ifs para verificar entrada do usuário

--- scripts/script.js
+++ scripts/script.js
@@ -12,63 +12,64 @@ let resultado = document.getElementById('resultado');
 const formulario = document.getElementById('formulario');
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-    formulario.addEventListener('submit', function(event) {
-      event.preventDefault();
+    formulario.addEventListener('submit', function (event) {
+        event.preventDefault();
     });
 
 });
 
-botao.addEventListener('click', 
-    function() {
+botao.addEventListener('click',
+    function () {
 
         let valor = inputValor.value;
         let moeda1 = input1.value;
-        let moeda2 = input2.value;        
-        
-        const url = ` https://economia.awesomeapi.com.br/last/${moeda1}-${moeda2}`;
+        let moeda2 = input2.value;
 
-        if(valor == 0){
-            alert('O Campo Valor não deve ficar em branco!');
-        }
+        if (valor <= 0) {
+            alert('O Campo "Valor" não deve ficar em branco ou ser um valor negativo!');
+            resultado.textContent = 'Erro na Conversão';
+        } else {
 
-        if(valor.value != 0 && moeda1 == moeda2){
-            alert('Não se pode converter valores pela mesma moeda. Escolha moedas diferentes!');
-        }
-        
-        fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                console.log('Erro na resposta da api');
-            }
-            return response.json();
-        })
-        .then(data => {
-            
-            let conversao = moeda1+moeda2;
-            let nomeObjson = Object.keys(data)[0];
-
-            let stringRetornada = data[nomeObjson].bid;
-            
-            let stringTonumber = parseFloat(stringRetornada);
-
-            let valorFormatado = stringTonumber.toFixed(2);
-
-            let saida;
-
-            if (conversao == nomeObjson) {
-
-                saida = (valorFormatado * valor);
-
-                let resultFinal = moeda2 + ': ' + saida.toFixed(2);
-
-                resultado.textContent = resultFinal;
-
-            } else{
+            if (valor > 0 && moeda1 == moeda2 || moeda1 == 'null' || moeda2 == 'null') {
+                alert('Escolha moedas diferentes para realizar a conversão!');
                 resultado.textContent = 'Erro na Conversão';
             }
+        }
 
-        })
+        if (valor > 0 && moeda1 != moeda2 && moeda1 != 'null' && moeda2 != 'null') {
+
+            const url = ` https://economia.awesomeapi.com.br/last/${moeda1}-${moeda2}`;
+
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        console.log('Erro na resposta da api');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+
+                    let conversao = moeda1 + moeda2;
+                    let nomeObjson = Object.keys(data)[0];
+                    let stringRetornada = data[nomeObjson].bid;
+                    let calculoConversao;
+                    let resultFinal;
+
+                    if (conversao == nomeObjson) {
+
+                        calculoConversao = (stringRetornada * valor);
+
+                        resultFinal = moeda2 + ': ' + calculoConversao.toFixed(2);
+
+                        resultado.textContent = resultFinal;
+
+                    } else {
+                        resultado.textContent = 'Api não permite essa Conversão';
+                    }
+                })
+        }
+
     }
 )
